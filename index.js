b571f62eabe344689a3eda10296bf243465fc5bf
Abzed <ibraabzed@gmail.com>
2022-03-21 12:59:38 +0300

    commit tree 53dae6fc38e799f92a5cc63e78312907fcf2126c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+let currentUser = 'Abzed Tet';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
