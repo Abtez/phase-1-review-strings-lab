e5807bb644b9017b6d9d21037869649b15410cff
abzed <ibraabzed@gmail.com>
2022-12-01 12:59:48 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser ="Abzed"
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+
+console.log(currentUser.charAt(0));
